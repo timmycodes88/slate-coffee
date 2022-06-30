f19f6dd02f14b2d916c3dfa5e17b99f787712681
Tim <tim@brightsidedeveloper.com>
2022-06-30 17:13:32 -0500

    Slide Show Testomonials

--- assets/global.js
+++ assets/global.js
@@ -626,3 +626,27 @@ class VariantRadios extends VariantSelects {
 }
 
 customElements.define('variant-radios', VariantRadios);
+
+$('.slide-show-cards').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: false,
+  responsive: [
+{
+  breakpoint: 989,
+  settings: {
+    slidesToShow: 2,
+    slidesToScroll: 1
+  }
+},
+{
+  breakpoint: 750,
+  settings: {
+    slidesToShow: 1,
+    slidesToScroll: 1
+  }
+}
+]
+});
